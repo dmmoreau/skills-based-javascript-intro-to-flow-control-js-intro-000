be1dbe124ad68938198b7fc9cf8131ddab137fc9
Dylan Moreau <1dylanmoreau@gmail.com>
2020-10-21 20:01:48 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,17 +1,40 @@
 function basicTeenager(age) {
-
+if (age === 13,14,15,16,17,18,19) {
+  return "You are a teenager!"
+} else {
+return undefined
+}
 }
 
 function teenager(age) {
+if (age === 13,14,15,16,17,18,19) {
+  return 'You are a teenager!'
+} else if (age === !13,!14,!15,!16,!17,!18,!19) {
 
+} {
+  return "You are not a teenager"
+}
 }
 
 function ageChecker(age) {
+if (age === 13,14,15,16,17,18,19) {
+  return 'You are a teenager!'
+} else if (age <=12) {
+  return "you are a kid"
+} else if (age >=20) {
+  return 'You are a grownup'
+}
+{
 
+}
 }
 
 function ternaryTeenager(age) {
-
+if (age === 13,14,15,16,17,18,19) {
+  return 'You are a teenager'
+} else {
+  return 'You are not a teenager'
+}
 }
 
 function switchAge(age) {
