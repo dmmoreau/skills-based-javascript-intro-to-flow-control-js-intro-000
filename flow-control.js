71839032ed96b0672dce4244ca90b8f67d7c5837
Dylan Moreau <1dylanmoreau@gmail.com>
2020-10-21 20:18:36 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,8 +1,8 @@
 function basicTeenager(age) {
 if (age === 13,14,15,16,17,18,19) {
   return "You are a teenager!"
-} else {
-return undefined;
+} else if ( age === <13 && >19) {
+return undefined
 }
 }
 
